2f02d59673fbeade9da426b032c241b74bf2da50
lucasvickers <lucasvickers@lucasvickerss-MacBook-Pro.local>
2014-04-08 23:44:20 -0400

    init commit

--- src/DepthRecorderApp.cpp
+++ src/DepthRecorderApp.cpp
@@ -1,20 +1,33 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "CinderOpenNI.h"
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
 class DepthRecorderApp : public AppNative {
-  public:
+public:
+    void prepareSettings(Settings *settings);
 	void setup();
-	void mouseDown( MouseEvent event );	
+	void mouseDown( MouseEvent event );
 	void update();
 	void draw();
+    void shutdown();
+private:
+    ci::openni::Camera camera;
 };
+
+void DepthRecorderApp::prepareSettings(Settings *settings)
+{
+    settings->setWindowSize( 1280, 480 );
+    settings->setFrameRate( 30 );
+}
 
 void DepthRecorderApp::setup()
 {
+    camera.setup( ci::openni::Camera::SENSOR_DEPTH | ci::openni::Camera::SENSOR_COLOR );
 }
 
 void DepthRecorderApp::mouseDown( MouseEvent event )
@@ -23,12 +36,26 @@ void DepthRecorderApp::mouseDown( MouseEvent event )
 
 void DepthRecorderApp::update()
 {
+    camera.update();
 }
 
 void DepthRecorderApp::draw()
 {
 	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
+    
+    
+    gl::draw( camera.getDepthTex(), Rectf( 0, 0, 640, 480 ) );
+    //    gl::draw( camera.getRawDepthTex(), Rectf( 0, 0, 640, 480 ) );
+    //    gl::draw( camera.getColorTex(), Rectf( 640, 0, 1280, 480 ) );
+    
+}
+
+void DepthRecorderApp::shutdown()
+{
+    console() << "Shutting down" << endl;
+    camera.close();
+    camera.shutdown();
 }
 
 CINDER_APP_NATIVE( DepthRecorderApp, RendererGl )
